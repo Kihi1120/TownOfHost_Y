14075f9a5a7963334845efd233679b3a96e80cf1
Kihi1120 <hope.light1120@gmail.com>
2024-03-02 03:04:23 +0900

    stringの追加（翻訳） 設定項目の数値がおかしいのを修正

--- Roles/Impostor/Y/NightMare.cs
+++ Roles/Impostor/Y/NightMare.cs
@@ -39,17 +39,17 @@ public sealed class NightMare : VoteGuesser, IImpostor
         NightMareNormalKillCrewVision,
         NightMareDarkSeconds,
     }
-    private float SpeedInLightsOut;          //停電時の移動速度
+    private float SpeedInLightsOut;          //停電時の移動速度の加速値
     private float KillCooldownInLightsOut;   //停電時のキルクール
     private float NormalKillCrewVision;　　　 //通常キル時のクルー陣営の視界
     private float DarkSeconds;               //通常キル時の暗転する秒数
     public static void SetupOptionItem()
     {
-        OptionSpeedInLightsOut = FloatOptionItem.Create(RoleInfo, 10, OptionName.NightMareSpeedInLightsOut, new(0.1f, 0.5f, 0.1f), 0.3f, false)
+        OptionSpeedInLightsOut = FloatOptionItem.Create(RoleInfo, 10, OptionName.NightMareSpeedInLightsOut, new(1.2f, 10.0f, 0.2f), 1.2f, false)
             .SetValueFormat(OptionFormat.Multiplier);
-        OptionKillCooldownInLightsOut = FloatOptionItem.Create(RoleInfo, 11, OptionName.NightMareKillCooldownInLightsOut, new(2.5f, 180f, 2.5f), 15f, false)
+        OptionKillCooldownInLightsOut = FloatOptionItem.Create(RoleInfo, 11, OptionName.NightMareKillCooldownInLightsOut, new(0.0f, 180f, 2.5f), 15f, false)
             .SetValueFormat(OptionFormat.Seconds);
-        OptionNormalKillCrewVision = FloatOptionItem.Create(RoleInfo, 12, OptionName.NightMareNormalKillCrewVision, new(0f, 5f, 0.25f), 2f, false)
+        OptionNormalKillCrewVision = FloatOptionItem.Create(RoleInfo, 12, OptionName.NightMareNormalKillCrewVision, new(0.0f, 3.0f, 0.1f), 0.1f, false)
             .SetValueFormat(OptionFormat.Multiplier);
         OptionDarkSeconds = FloatOptionItem.Create(RoleInfo, 13, OptionName.NightMareDarkSeconds, new(0, 100, 1), 7, false)
         .SetValueFormat(OptionFormat.Seconds);
