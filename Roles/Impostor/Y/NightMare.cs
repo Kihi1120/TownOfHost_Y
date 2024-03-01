01852da15276828406b2d761e8f60d33bc87c95f
Kihi1120 <hope.light1120@gmail.com>
2024-03-02 02:04:21 +0900

    設定項目辺り。

--- Roles/Impostor/Y/NightMare.cs
+++ Roles/Impostor/Y/NightMare.cs
@@ -14,7 +14,7 @@ public sealed class NightMare : VoteGuesser, IImpostor
             CustomRoles.NightMare,
             () => RoleTypes.Impostor,
             CustomRoleTypes.Impostor,
-            (int)Options.offsetId.ImpY + 1400,
+            (int)Options.offsetId.ImpY + 1500,
             SetupOptionItem,
             "ナイトメアー"
         );
@@ -23,12 +23,35 @@ public sealed class NightMare : VoteGuesser, IImpostor
         RoleInfo,
         player)
     {
-
+        SpeedInLightsOut = OptionSpeedInLightsOut.GetFloat();
+        KillCooldownInLightsOut = OptionKillCooldownInLightsOut.GetFloat();
+        NormalKillCrewVision = OptionNormalKillCrewVision.GetFloat();
+        DarkSeconds = OptionDarkSeconds.GetFloat();
     }
+    private static OptionItem OptionKillCooldownInLightsOut;
+    private static OptionItem OptionSpeedInLightsOut;
+    private static OptionItem OptionNormalKillCrewVision;
+    private static OptionItem OptionDarkSeconds;
     enum OptionName
     {
+        NightMareSpeedInLightsOut,
+        NightMareKillCooldownInLightsOut,
+        NightMareNormalKillCrewVision,
+        NightMareDarkSeconds,
     }
+    private float SpeedInLightsOut;          //停電時の移動速度
+    private float KillCooldownInLightsOut;   //停電時のキルクール
+    private float NormalKillCrewVision;　　　 //通常キル時のクルー陣営の視界
+    private float DarkSeconds;               //通常キル時の暗転する秒数
     public static void SetupOptionItem()
     {
+        OptionSpeedInLightsOut = FloatOptionItem.Create(RoleInfo, 10, OptionName.NightMareSpeedInLightsOut, new(0.1f, 0.5f, 0.1f), 0.3f, false)
+            .SetValueFormat(OptionFormat.Multiplier);
+        OptionKillCooldownInLightsOut = FloatOptionItem.Create(RoleInfo, 11, OptionName.NightMareKillCooldownInLightsOut, new(2.5f, 180f, 2.5f), 15f, false)
+            .SetValueFormat(OptionFormat.Seconds);
+        OptionNormalKillCrewVision = FloatOptionItem.Create(RoleInfo, 12, OptionName.NightMareNormalKillCrewVision, new(0f, 5f, 0.25f), 2f, false)
+            .SetValueFormat(OptionFormat.Multiplier);
+        OptionDarkSeconds = FloatOptionItem.Create(RoleInfo, 13, OptionName.NightMareDarkSeconds, new(0, 100, 1), 7, false)
+        .SetValueFormat(OptionFormat.Seconds);
     }
 }
