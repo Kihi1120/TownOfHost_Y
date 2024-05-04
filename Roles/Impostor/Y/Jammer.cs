31d6992105fe13aa460ba38b15a165c4ceba732a
Kihi1120 <hope.light1120@gmail.com>
2024-05-04 17:48:23 +0900

    <設定項目作り。> 　キルクール 　指名できる回数 　減速値 　サボ中の減速する持続時間 　シェイプ時の減速する持続時間

--- Roles/Impostor/Y/Jammer.cs
+++ Roles/Impostor/Y/Jammer.cs
@@ -23,11 +23,44 @@ public sealed class Jammer : RoleBase, IImpostor
         player
     )
     {
+        KillCooldown = OptionKillCooldown.GetFloat();
+        ShapeshiftCount = OptionShapeshiftCount.GetInt();
+        DownSpeed = OptionDownSpeed.GetFloat();
+        SaboDownSpeedTime = OptionSaboDownSpeedTime.GetFloat();
+        ShapeDownSpeedTime = OptionShapeDownSpeedTime.GetFloat();
     }
+    private static OptionItem OptionKillCooldown;
+    private static OptionItem OptionShapeshiftCount;
+    public static OptionItem OptionDownSpeed;
+    private static OptionItem OptionSaboDownSpeedTime;
+    private static OptionItem OptionShapeDownSpeedTime;
+
     enum OptionName
     {
+        JammerShapeshiftMaxCount,
+        JammerDownSpeed,
+        JammerSaboDownSpeedTime,
+        JammerShapeDownSpeedTime,
     }
+    private static float KillCooldown;
+    int ShapeshiftCount;
+    private static float DownSpeed;
+    private static float SaboDownSpeedTime;
+    private static float ShapeDownSpeedTime;
+
+
     private static void SetUpOptionItem()
     {
+        //OptionCanDeadReport = BooleanOptionItem.Create(RoleInfo, 10, OptionName.ShapeKillerCanDeadReport, true, false);
+        OptionKillCooldown = FloatOptionItem.Create(RoleInfo, 10, GeneralOption.KillCooldown, new(2.5f, 180f, 2.5f), 30f, false)
+                .SetValueFormat(OptionFormat.Seconds);
+        OptionShapeshiftCount = IntegerOptionItem.Create(RoleInfo, 11, OptionName.JammerShapeshiftMaxCount, new(1, 3, 1), 1, false)
+        .SetValueFormat(OptionFormat.Times);
+        OptionDownSpeed = FloatOptionItem.Create(RoleInfo, 12, OptionName.JammerDownSpeed, new(0.1f, 1f, 0.1f), 1f, false)
+               .SetValueFormat(OptionFormat.Multiplier);
+        OptionSaboDownSpeedTime = FloatOptionItem.Create(RoleInfo, 13, OptionName.JammerSaboDownSpeedTime, new(1f, 180f, 1f), 5f, false)
+               .SetValueFormat(OptionFormat.Seconds);
+        OptionShapeDownSpeedTime = FloatOptionItem.Create(RoleInfo, 14, OptionName.JammerShapeDownSpeedTime, new(1f, 180f, 1f), 5f, false)
+        .SetValueFormat(OptionFormat.Seconds);
     }
 }
